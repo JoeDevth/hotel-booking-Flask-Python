aadc50e1057274576557a6f93fc3dd76f886609a
JoeDevth <643070310021@npu.ac.th>
2024-09-27 08:44:43 +0700

    update booking_html

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+# models.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -5,6 +6,7 @@ db = SQLAlchemy()
 class Room(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+    is_booked = db.Column(db.Boolean, default=False)
 
 class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,3 +15,12 @@ class Employee(db.Model):
 class Customer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
+
+class Booking(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    room_id = db.Column(db.Integer, db.ForeignKey('room.id'), nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+    check_in_date = db.Column(db.DateTime, nullable=False)
+    check_out_date = db.Column(db.DateTime, nullable=False)
+    room = db.relationship('Room', backref='bookings')
+    customer = db.relationship('Customer', backref='bookings')
